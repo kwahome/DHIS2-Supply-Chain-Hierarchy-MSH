52acdd7bc6bf816887583c260caed57e5075e96c
kwahome <kevowahome@gmail.com>
2015-06-11 09:21:32 +0300

    User guide incorporated into the Web  APP

---
+++ client/templates/userguide.php
@@ -0,0 +1,38 @@
+                    <div class = "col-md-3">
+                        <span style = "color:blue;margin-bottom:5px">Classifying facilities</span>
+                        <p class = "lead text-justify" style = "font-size:10pt">
+                            Classify facilities as Central Stores, Satellite Sites or Stand Alone Sites.
+                            Go to 
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFacilities">
+                            CLASSIFY FACILITIES
+                            </a>, click on the facility type to classify and select the facilities to classify under it.
+                            Classification of facilities establishes a hierarchy of the supply chain
+                            for report generation.
+                        </p>
+                    </div>
+                    <div class = "col-md-3">
+                        <span style = "color:blue;">Sorting using Org. Units</span>
+                        <p class = "lead text-justify" style = "font-size:10pt">
+                            There are over 9,474 facilities in the Kenya captured in DHIS2.
+                            It is impossible to consider all of them at once for classification.
+                            Use the 
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOrgUnits">
+                                ORGANIZATION UNITS
+                            </a>
+                            hierarchy to narrow down to low level units.
+                            Click on an organization unit to get a list of facilities under it.
+                        </p>
+                    </div>
+                    <div class = "col-md-3">
+                        <span style = "color:blue;">Updating the System</span>
+                        <p class = "lead text-justify" style = "font-size:10pt">
+                            There is need to query DHIS2 for newly created organization units.
+                            Use 
+                            <a  data-toggle="collapse" data-parent="#accordion" href="#collapseTUpdates">
+                                UPDATES
+                            </a> 
+                            to update the system database from DHIS2 Web API.
+                            Updates fetch new organization units that may have been created.
+
+                        </p>
+                    </div> 
